111cf52de3cd9b7e7ebb0c90d2b3ce63f7f91fa0
ozerikoz <erik.paschoalatto1@gmail.com>
2025-05-20 23:43:29 -0300

    feat: add OracleDbContext

---
+++ lugiatrack-api/Data/OracleDbContext.cs
@@ -0,0 +1,22 @@
+using lugiatrack_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace lugiatrack_api.Data;
+
+public class OracleDbContext : DbContext
+{
+    public DbSet<Funcionario> Funcionarios { get; set; }
+    public DbSet<Moto> Motos { get; set; }
+
+    public string? ConnectionString { get; set; }
+
+    public OracleDbContext()
+    {
+        ConnectionString = System.Configuration.ConfigurationManager
+                               .AppSettings["FiapOracleDb"] ?? 
+                           "Data Source=//oracle.fiap.com.br:1521/orcl;User Id=rm554854; Password=090304;";
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+        options.UseOracle(ConnectionString);
+}
